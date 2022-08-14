fa71af63c7e29dfd2c387f88e5e2b3c023a13735
ABOAO <lawliet0913@gmail.com>
2022-08-14 21:42:10 +0800

    2022/08/13

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Astral by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -208,7 +203,6 @@
 				});
 
 		}
-	//Contact
-
-
+		
 })(jQuery);
+
